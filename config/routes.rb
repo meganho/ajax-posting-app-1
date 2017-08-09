79e016fae51f6f31609189019fa393be2ab7c90b
meganho <bbeeaanns@gmail.com>
2017-08-09 17:47:25 +0200

    ch4

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
      member do
        post "like" => "posts#like"
        post "unlike" => "posts#unlike"
+       post "toggle_flag" => "posts#toggle_flag"
+       post "rate" => "posts#rate"
+
    end
       end
 
---
+++ db/migrate/20170809140028_add_flag_to_posts.rb
@@ -0,0 +1,8 @@
+class AddFlagToPosts < ActiveRecord::Migration[5.0]
+  def change
+        add_column :users, :role, :string
+        add_column :posts, :flag_at, :datetime
+
+    
+  end
+end
